a7325ef6590e2a301ae6f3bfd5a1463aba79025a
Dayoung <dayoung.kim@lincolnuni.ac.nz>
2025-12-16 17:15:43 +1300

    module example

---
+++ module.py
@@ -0,0 +1,8 @@
+def price(people):
+    print(f"{people} people: price is {people * 10000} won.")
+
+def price_morning(people):
+    print(f"{people} people: morning price is {people * 6000} won.")
+
+def price_soldier(people):
+    print(f"{people} people: soldier price is {people * 4000} won.")
---
+++ module_use.py
@@ -0,0 +1,7 @@
+# import module as m
+from module import *
+
+price(3)
+price_morning(4)
+price_soldier(5)
+
