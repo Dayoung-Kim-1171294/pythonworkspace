88f1d1aa1e2488b3d69cc2d4d8426dada488a555
Dayoung <dayoung.kim@lincolnuni.ac.nz>
2025-12-16 17:45:47 +1300

    module with execution context and import behavior

--- package.py
+++ package.py
@@ -1,3 +1,4 @@
+# Once you import the file, the code in the file is executed.(Thailand file executed)
 import travel.thailand
 # use basic constructor that comes from parent class(Object class)
 trip_to = travel.thailand.ThailandPackage()
--- travel/thailand.py
+++ travel/thailand.py
@@ -1,3 +1,12 @@
 class ThailandPackage:
     def detail(self):
         print("Thailand 5 nights 6 days package")   
+
+print("__name__:", __name__)
+if __name__ == "__main__":
+    print("Thailand module is being run directly")
+    trip = ThailandPackage()
+    trip.detail()
+else:
+    #__name__: travel.thailand
+    print("Thailand module imported")
